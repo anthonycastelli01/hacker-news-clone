fa2f26d6f427e093e984400c8d37812f1e435ac2
Anthony Castelli <anthonycastelli01@gmail.com>
2015-09-22 10:07:17 -0700

    Add migrations

---
+++ db/migrate/20150922100420_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.string :votable_type
+      t.integer :votable_id
+      t.references :user
+      t.integer :value
+
+      t.timestamps null: false
+    end
+  end
+end
