2a6b7c22915c736a71dd4d64b3dc078fd568553b
Anthony Castelli <anthonycastelli01@gmail.com>
2015-09-24 08:59:55 -0700

    Implement most of project

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,11 +1,27 @@
+get '/users/login' do
+  erb :'_login_form', layout: false
+end
+
 post '/sessions/create' do
   user = User.find_by_username(params[:username])
+  puts "*" * 60
+  puts "Creating session..."
   if user.nil?
-    redirect '/'
+    puts "*" * 60
+    puts "NOPE"
+    return "<p>Sorry, those aren't the right values.</p>"
   elsif user.password == params[:password]
+    puts "*" * 60
+    puts "GOOD JOB"
     session[:user_id] = user.id
-    redirect "/users/#{session[:user_id]}"
+    return "<p>Welcome, #{user.username}!</p>"
   else
-    redirect '/'
+    puts "*" * 60
+    puts "WTF"
+    return "<p>I have no idea what you just did.</p>"
   end
+end
+
+get '/users/new' do
+  erb :'_registration_form', layout: false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,60 @@
 $(document).ready(function() {
-  //
+  $(".login-link").on("click", add_login_form);
+  $(".login-box").on('submit', log_user_in);
+  $(".registration-link").on("click", add_registration_form);
+  $(".registration-box").on('submit', create_new_user)
 });
+
+var add_login_form = function(event) {
+  event.preventDefault();
+  var login_div = this;
+
+  $.ajax({
+    type: "get",
+    url: "/users/login",
+  }).done(function(response) {
+    if ($(".login-box").children().length == 0) {
+      $(".login-box").append(response);
+    } else {
+      $(".login-box").html(response);
+    }
+  })
+}
+
+var log_user_in = function(event) {
+  event.preventDefault();
+  var form = $(this).children();
+  var path = form.attr("action");
+  var method = form.attr("method");
+  var loginInfo = form.serialize();
+  var loginForm = this;
+
+  $.ajax({
+    type: method,
+    url: path,
+    data: loginInfo
+  }).done(function(response) {
+    form.remove();
+    $('.login-box').append(response);
+  })
+}
+
+var add_registration_form = function(event) {
+  event.preventDefault();
+  var register_div = this;
+
+  $.ajax({
+    type: "get",
+    url: "/users/new",
+  }).done(function(response) {
+    if ($(".registration-box").children().length == 0) {
+      $(".registration-box").append(response);
+    } else {
+      $(".registration-box").html(response);
+    }
+  })
+}
+
+var create_new_user = function(event) {
+  event.preventDefault();
+}
